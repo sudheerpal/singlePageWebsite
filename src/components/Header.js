994d7ddf4fb845a1a564c02e172e70551475b557
Abdul Basir <basir.bsmrstu@gmail.com>
2022-11-13 15:27:13 +0600

    content added

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ export default function Header() {
         <div className='bg-light'>
             <div className="container py-5">
                 <Link className='nav-link' to={'/'}>
-                    <h4 className='text-uppercase'>  <span className='bg-danger text-white '>Globalhealthcare</span><span className='bg-dark text-danger'>reports</span><span className='text-muted'></span> </h4>
+                    <h4 className='text-uppercase'>  <span className='bg-danger text-white px-1'>Globalhealthcare</span><span className='bg-dark text-danger px-1'>reports</span><span className='text-muted'></span> </h4>
                 </Link>
-                <div className='col-md-10 col-lg-8 mt-3 col-12'>Year On Year Markets accumulate and examine raw data across 12 major B2B verticals to provide market insight that enables companies to get competitive edge and detect market disruptions early.
+                <div className='col-md-10 col-lg-8 mt-3 col-12'>Global healthcare Reports accumulate and examine raw data across 12 major B2B verticals to provide market insight that enables companies to get competitive edge and detect market disruptions early.
                 </div>
             </div>
         </div>
